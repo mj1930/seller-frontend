17e2aa0e5cbe9b7b905d5d9880e8bdfe474a129f
Mihir Jain <mihir@chingari.io>
2021-03-07 19:29:55 +0530

    added changes

--- src/app/seller/seller-profile-page/seller-profile-page.component.ts
+++ src/app/seller/seller-profile-page/seller-profile-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SellerService } from '../seller.service';
+import { VerificationService } from '../verification.service';
 
 @Component({
   selector: 'app-seller-profile-page',
@@ -8,13 +9,33 @@ import { SellerService } from '../seller.service';
 })
 export class SellerProfilePageComponent implements OnInit {
   user:any;
-
-  constructor(private sellerService: SellerService) { }
+  mailVerified = false;
+  userName: any;
+  constructor(
+    private sellerService: SellerService,
+    private verificationService: VerificationService
+    ) { }
 
   ngOnInit(): void {
+    this.mailVerified = localStorage.getItem('verified') ? Boolean(localStorage.getItem('verified')): false;
+    this.userName = JSON.parse(localStorage.getItem('user')).name;
     this.getUserDetails();
   }
 
+  sentMail = false;
+  sendVerificationEmail() {
+    let reqBody = {
+      email: JSON.parse(localStorage.getItem('user')).email
+    } ;
+    this.sentMail = true;
+    this.verificationService.sendVerificationEmail(reqBody).subscribe(data => {
+
+      console.log(data);
+    }, error => {
+      console.log(error);
+    })
+  }
+  
   getUserDetails() {
   this.sellerService.getUserDetails().subscribe(data => {
     console.log(data);
