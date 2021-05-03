778540be1dc97b29f329ca3718d8dc25ac76e5ec
Ritesh Kumar1 <ritesh.kumar1@espire.com>
2021-05-03 19:54:45 +0530

    Manage Inventory functionality

--- src/app/inventory/manage-inventory/manage-inventory.component.ts
+++ src/app/inventory/manage-inventory/manage-inventory.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import * as $ from 'jquery';
 import { ProductService } from '../../product/product.service';
+import { ToastService } from "src/app/services/shared/toast.service";
+
 @Component({
   selector: 'app-manage-inventory',
   templateUrl: './manage-inventory.component.html',
@@ -13,7 +15,10 @@ export class ManageInventoryComponent implements OnInit {
   showSubMenu = false;
   userName = '';
 
-  constructor(private productService: ProductService, private router: Router) { }
+  constructor(
+    private productService: ProductService, 
+    private router: Router,
+    private toastService: ToastService) { }
 
   ngOnInit(): void {
     this.getProducts();
@@ -36,5 +41,31 @@ this.userName = JSON.parse(localStorage.getItem('user')).name;
 
   goToProdDescPage(id) {
     this.router.navigate(['/inventory/product-description', id])
+  }
+
+  editProduct(product_dtl) {
+    localStorage.setItem('product', JSON.stringify(product_dtl));
+    this.router.navigate(['/product/add-products']);
+  }
+
+  updatePrice(product_dtl) {
+    product_dtl.productId = product_dtl._id;
+    delete product_dtl._id;
+    delete product_dtl.userId;
+    delete product_dtl.productImg;
+    delete product_dtl.unitCount;
+    delete product_dtl.isApproved;
+    delete product_dtl.feedback;
+    delete product_dtl.isDeleted;
+    delete product_dtl.createdAt;
+    delete product_dtl.updatedAt;
+    delete product_dtl.__v;
+    this.productService.editProduct(product_dtl).subscribe(
+      data => {
+        this.toastService.openSnackbar("Price Updated succeefully!!");
+      },
+      error => {
+        console.log(error);
+      });
   }
 }
