54804e0d31a01efaa469cc144517c2c8fdc2b309
Mihir Jain <mj3003@Mihirs-MacBook-Air.local>
2021-05-16 13:32:47 +0530

    changes with dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://6arhtir1m7.execute-api.ap-south-1.amazonaws.com/',
-  adminApiUrl: 'https://rgr3szfd44.execute-api.ap-south-1.amazonaws.com/'
+  apiUrl: 'http://65.2.73.90:3003/',
+  adminApiUrl: 'http://65.2.31.91:3002/'
 };
